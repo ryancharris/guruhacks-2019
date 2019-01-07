e09d1a9393a50dbbbf9324c06e3980438c81bcf2
Ryan Harris <harrisryan1@gmail.com>
2019-01-07 15:43:55 -0500

    Initial commit

---
+++ src/WorldMap.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./WorldMap.css";
+
+class WorldMap extends Component {
+  render() {
+    return (
+      <div className="WorldMap">
+        <header className="WorldMap-header">
+          <p>Hi</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default WorldMap;
