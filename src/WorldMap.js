cec9d618777f1cd2c865be919d15b5755949deed
Ryan Harris <harrisryan1@gmail.com>
2019-01-07 16:13:42 -0500

    Get a map to render like a baller

--- src/WorldMap.js
+++ src/WorldMap.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+
+import * as d3 from "d3";
+import { feature } from "topojson-client";
+
 import "./WorldMap.css";
+import worldMap from "./json/worldMap.json";
+
+const projection = d3.geoEquirectangular();
+const path = d3.geoPath().projection(projection);
+const countries = feature(worldMap, worldMap.objects.countries).features;
 
 class WorldMap extends Component {
   render() {
+    console.log(worldMap);
     return (
       <div className="WorldMap">
-        <header className="WorldMap-header">
-          <p>Hi</p>
-        </header>
+        <svg width={960} height={500}>
+          <g>
+            {countries.map(country => {
+              console.log(country);
+              return <path className="country" d={path(country)} />;
+            })}
+          </g>
+        </svg>
       </div>
     );
   }
