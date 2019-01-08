13a2a26fa87343ae238c8c051e46b4915854aca1
Ryan Harris <harrisryan1@gmail.com>
2019-01-08 10:29:39 -0500

    Properly plot map points

--- src/WorldMap.js
+++ src/WorldMap.js
@@ -1,34 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import * as d3 from "d3";
 import { feature } from "topojson-client";
 
 import "./WorldMap.css";
 import worldMap from "./json/worldMap.json";
+import roiOutput from "./json/roiOutput.json";
 
-const projection = d3.geoEquirectangular();
+const mapCenter = d3.geoCentroid(feature(worldMap, worldMap.objects.countries));
+const projection = d3
+  .geoEquirectangular()
+  .scale(150)
+  .translate([950 / 2, 500 / 2]);
 const path = d3.geoPath().projection(projection);
 const countries = feature(worldMap, worldMap.objects.countries).features;
 
 class WorldMap extends Component {
-  projection() {
-    return d3
-      .geoEquirectangular()
-      .scale(150)
-      .translate([950 / 2, 500 / 2]);
-  }
-
-  componentDidMount() {
-    console.log(worldMap);
-  }
-
   render() {
-    const coordinates = [-43.1729, -22.9068];
-
     return (
       <div className="WorldMap">
         <svg
           id="mapSvg"
+          // width="950"
+          // height="500"
           viewBox="0 0 950 500"
           xmlns="http://www.w3.org/2000/svg"
         >
@@ -38,11 +32,33 @@ class WorldMap extends Component {
             })}
           </g>
           <g className="markers">
-            <circle
+            {roiOutput.map(event => {
+              const originCoordinates = [event.olon, event.olat];
+              const destinationCoordinates = [event.dlon, event.dlat];
+              return (
+                <Fragment>
+                  <circle
+                    cx={projection(originCoordinates)[0]}
+                    cy={projection(originCoordinates)[1]}
+                    r={2}
+                    fill={"red"}
+                    className="origin-point"
+                  />
+                  <circle
+                    cx={projection(destinationCoordinates)[0]}
+                    cy={projection(destinationCoordinates)[1]}
+                    r={2}
+                    fill={"purple"}
+                    className="destination-point"
+                  />
+                </Fragment>
+              );
+            })}
+            {/* <circle
               cx={this.projection()(coordinates)[0]}
               cy={this.projection()(coordinates)[1]}
               r={2}
-            />
+            /> */}
           </g>
         </svg>
       </div>
